841eb8ff2a080cb1ce89a9b57251e02de413f891
Lee Jia Yee <yee00@graduate.utm.my>
2022-12-06 15:04:46 +0800

    upload el edited welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,8 @@
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <!-- @if (Route::has('login')) -->
         <div class="hidden fixed top-0 left-0 px-6 py-4 sm:block">
-            <!-- @auth -->
-            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500">Dashboard</a>
+            {{-- <!-- @auth -->
+            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500">Dashboard</a> --}}
             <!-- @else -->
             <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500">Log in</a>
 
